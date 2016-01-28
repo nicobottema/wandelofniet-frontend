348b5e006ef836ae009675982118b592bcce98b9
Rutger <rutger.knobbe@gmail.com>
2016-01-28 23:44:03 +0100

    final version 1.0

--- pages/buymovie.inc.php
+++ pages/buymovie.inc.php
@@ -2,7 +2,7 @@
 	// var_dump($_SESSION["user"]->_id);
 	// die();
 	$fields = array(
-		'description' => urlencode($_GET['id']),
+		'desc' => urlencode($_GET['id']),
 		'amount' => 9.95,
 		'user_id' => $_SESSION["user"]->_id
 	);
@@ -29,12 +29,37 @@
 
 	<?php 
 	if($json->success) {
-		?>
-			<h2> Bedankt voor de aankoop </h2>
-		<?php
+		
+		$fields = array(
+			'username' => urlencode($_SESSION['user']->username),
+			'password' => urlencode($_SESSION['user']->password)
+		);
+		$fields_string = "";
+		foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+		rtrim($fields_string, '&');
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "http://".$apiHost . ":" . $apiPort . "/users/login");
+		curl_setopt($ch,CURLOPT_POST, count($fields));
+		curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$output = curl_exec($ch);
+		curl_close($ch);
+		$json = json_decode($output);
+		
+		if($json->success) {
+			$_SESSION["user"] = $json->user;
+
+
+			?>
+				<h2> Bedankt voor de aankoop </h2>
+			<?php
+		} else {
+			?>
+				<h2>Er is iets mis gegaan. Onze excuses voor het ongemak</h2>
+			<?php 
+		}
 	} else {
-		//$error = $json->message;
-		//echo $error;
 		?>
 			<h2>Er is iets mis gegaan. Onze excuses voor het ongemak</h2>
 		<?php 
@@ -43,4 +68,3 @@
 			<a href="./"><div class="btn-class">Back to start</div></a>
 		</div>
 	</div>
-?>
